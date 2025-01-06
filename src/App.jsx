94b0c8586fee5f60a3e33c424ad6775ed9f0ef68
PRAVAT BEHERA <beherapravat301@gmail.com>
2025-01-06 20:32:03 +0530

    add utility functions, update styles, and configure shand CN ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+// import Header from './components/root/Header'
+import Searchbar from "./components/root/Searchbar";
+import { Button } from "./components/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className='text-center bg-blue-900 text-white p-4 text-4xl'> hello </div>
+      <div className="text-center bg-blue-900 text-white p-4 text-4xl">
+        {" "}
+        hello{" "}
+      </div>
+      {/* <Header/> */}
+      <Searchbar />
+    
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
